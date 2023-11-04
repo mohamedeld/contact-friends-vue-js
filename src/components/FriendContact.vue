05992853d3a35a4fd8a1740048c784aa1d174b9b
mohamedeld <mohamed.azoz20010@gmail.com>
2023-11-04 19:44:49 +0200

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,60 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)':'' }}</h2>
+    <button @click="toggleFavorite">Toggle Favorites</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      
+    };
+  },
+  props:{
+    id:{
+      type:String,
+      required:true
+    },
+    name:{
+      type:String,
+      required:true
+    },
+    phone:{
+      type:String,
+      required:true
+    },
+    email:{
+      type:String,
+      required:true
+    },
+    isFavorite:{
+      type:Boolean,
+      required:false,
+      default:false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(){
+      this.$emit('toggleFriendFavor',this.id)
+    }
+  },
+  emits:['toggleFavorite']
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="sendData">
+    <div>
+      <label>Name</label>
+      <input type="text" name="name" v-model="userName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="text" name="phone" v-model="userPhone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="text" name="email" v-model="userEmail" />
+    </div>
+    <div><button>Add Friend</button></div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    userName: "",
+    userPhone: "",
+    userEmail: "",
+    
+  }),
+  methods: {
+    sendData() {
+      let userData = {
+        name: this.userName,
+        phone: this.userPhone,
+        email: this.userEmail,
+        
+      };
+      this.$emit("addNewFriend", userData);
+     
+    },
+  },
+  emits: ["addNewFriend"],
+};
+</script>
+
+<style></style>
